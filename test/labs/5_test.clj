767d61830824d34d0e834de126489bc6cff6d5b9
Korovin Viktor <sarvash.inc@gmail.com>
2020-01-29 12:32:23 +0700

    5 lab (fix tests)

--- test/labs/5_test.clj
+++ test/labs/5_test.clj
@@ -11,9 +11,11 @@
 (test/deftest simple-test
   (test/testing "Ho Ho Ho IEEE754 is awesome"
     (test/is (< (abs (- 9 (integrate square 3))) 0.01))
-    (test/is (< (abs (- 81 (integrate square 9))) 0.01))
-    (test/is (< (abs (- 1 (integrate sin Math/PI))) 0.01))
-    (test/is (< (abs (- 0 (integrate sin (/ Math/PI 2)))) 0.01))
+    (test/is (< (abs (- 243 (integrate square 9))) 0.01))
+    (test/is (< (abs (- 2 (integrate sin Math/PI))) 0.01))
+    (test/is (< (abs (- 1 (integrate sin (/ Math/PI 2)))) 0.01))
 
     )
   )
+
+(simple-test)
